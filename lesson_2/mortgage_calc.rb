b0f9a2dd99782d4b2e81c5db704c80c736d57132
Pablo Vent <ventpdev@gmail.com>
2019-09-16 21:11:23 +0100

    monthly_payments format fixed.

--- lesson_2/mortgage_calc.rb
+++ lesson_2/mortgage_calc.rb
@@ -8,8 +8,8 @@ def messages(message, lang)
   MESSAGES[lang][message]
 end
 
-def prompt(message)
-  puts "=> #{message}"
+def prompt(message, msg2=nil)
+  puts "=> #{message}#{msg2}"
 end
 
 def validate_name?(name_str)
@@ -76,7 +76,7 @@ loop do
         prompt(messages('invalid_number', language))
       end
     end
-    interest_rate                                                                  
+    interest_rate                                                        
   end
   interest_rate = retrieve_inter_rate(language).to_f
 
@@ -86,7 +86,8 @@ loop do
       prompt(messages('loan_length', language))
       loan_duration = Kernel.gets().chomp().strip()
 
-      if validate_number?(loan_duration) && loan_duration.to_i <= 12 && loan_duration.to_i > 0 
+      if validate_number?(loan_duration) && loan_duration.to_i <= 12 \
+        && loan_duration.to_i > 0
         break
       elsif loan_duration == '0'
         prompt(messages('zero_division', language))
@@ -94,26 +95,27 @@ loop do
         prompt(messages('invalid_number', language))
       end
     end
-    loan_duration                                                              
+    loan_duration.to_i                                                      
   end
   loan_duration = retrieve_duration(language)
+
+  annual_interest_rate = interest_rate / 100
+  month_interest_rate = annual_interest_rate / 12
+  amount_in_months =  loan_duration * 12
+
+  monthly_payments = loan_amount * (month_interest_rate / \
+                                  (1 - (1 + month_interest_rate)**\
+                                  (- amount_in_months)))
+
+  # prompt("Your monthly payment is: £#{format('%02.2f', monthly_payments)}")
+  prompt(messages('instalment', language), format('%02.2f', monthly_payments))
 
   break
 end
 
+# m = p * ( j / ( 1 - ( 1 + j ) ** ( -n ) ) )
 
-# calculate:
-
-  # translate APR in months
-    # annual_interest = interest_rate / 100
-    # month_interest = annual_interest / 12
-
-  # translate loan duration into months
-    # amount_in_months =  loan_duration * 12
-
-  #  calculate monthly payments => m = p * ( j / ( 1 - ( 1 + j ) ** ( -n ) ) )
-
-  # m = monthly payments                 => to be calculated
-  # p = loan amount                           => amount_in_months
-  # j = monthly interest                    => month_interest
-  # n = loan duration in months         => amount_in_months
+# m = monthly payments                 => to be calculated
+# p = loan amount                           => amount applied for
+# j = monthly interest                    => month_interest
+# n = loan duration in months         => amount_in_months
